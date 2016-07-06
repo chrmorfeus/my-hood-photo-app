c80f955e3de44667dbeffc04917b50232eb43fc5
Christian Morera Figueroa <christianmorerafigueroa@Christians-MacBook.local>
2016-07-06 15:45:38 -0600

    first commit

---
+++ my-hood/DataServices.swift
@@ -0,0 +1,9 @@
+//
+//  DataServices.swift
+//  my-hood
+//
+//  Created by Christian Morera Figueroa on 7/5/16.
+//  Copyright © 2016 Christian Morera Figueroa. All rights reserved.
+//
+
+import Foundation
---
+++ my-hood/post.swift
@@ -0,0 +1,9 @@
+//
+//  post.swift
+//  my-hood
+//
+//  Created by Christian Morera Figueroa on 7/4/16.
+//  Copyright © 2016 Christian Morera Figueroa. All rights reserved.
+//
+
+import Foundation
